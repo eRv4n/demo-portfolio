24c8a270ba9e5020552a6caa0e178d9e381fddff
Rvann <ervanspprtdv@gmail.com>
2023-07-19 19:40:48 +0700

    Added responsive navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import "./Navbar.scss";
 import "../../css/_utility.scss";
 import { NavLink } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Navbar = () => {
   const navbar = [
@@ -18,19 +18,30 @@ const Navbar = () => {
     console.log(themeMode);
   };
 
-  if (themeMode === "light_mode") {
-    document.querySelector("body").style.backgroundColor = "#212529";
-    document.querySelector("body").style.color = "#f8f9fa";
-  } else {
-    document.querySelector("body").style.backgroundColor = "#f8f9fa";
-    document.querySelector("body").style.color = "#212529";
-  }
+  useEffect(() => {
+    if (themeMode === "light_mode") {
+      document.querySelector("body").style.backgroundColor = "#212529";
+      document.querySelector("body").style.color = "#f8f9fa";
+      document.querySelector(".re-nav-wrap").style.backgroundColor = "#212529";
+    } else {
+      document.querySelector("body").style.backgroundColor = "#f8f9fa";
+      document.querySelector("body").style.color = "#212529";
+      document.querySelector(".re-nav-wrap").style.backgroundColor = "#f8f9fa";
+    }
+  }, [themeMode]);
+
+  const [showNav, setShowNav] = useState("show");
+
+  const test = () => {
+    showNav === "show" ? setShowNav("not-show") : setShowNav("show");
+    console.log(showNav);
+  };
 
   return (
     <>
       <nav className={`navbar bg-${themeMode}`}>
         <div className={`nav-wrap container`}>
-          <h1 className="logo">
+          <h1 className="nav-brand">
             R<span>Creative</span>.
           </h1>
           <div className="link">
@@ -45,6 +56,32 @@ const Navbar = () => {
               {themeMode}
             </button>
           </span>
+          <div className="nav-responsive">
+            <button class="material-symbols-outlined" for="nav-responsive" onClick={test}>
+              menu
+            </button>
+            <div className={`re-nav-wrap ${showNav}`}>
+              <div className="close">
+                <button class="material-symbols-outlined" for="nav-responsive" onClick={test}>
+                  close
+                </button>
+              </div>
+              <div className="re-item">
+                <div className="re-link">
+                  {navbar.map((item) => (
+                    <NavLink key={item.name} to={item.href} onClick={test}>
+                      {item.name}
+                    </NavLink>
+                  ))}
+                </div>
+                <span className="re-theme">
+                  <button class={`material-symbols-outlined bg-${themeMode}-nav`} onClick={themeToggle}>
+                    {themeMode}
+                  </button>
+                </span>
+              </div>
+            </div>
+          </div>
         </div>
       </nav>
     </>
